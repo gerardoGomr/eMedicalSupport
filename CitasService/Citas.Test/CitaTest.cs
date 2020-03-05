c037e25e88065623617cc431a9eda5fcaa6b9eef
Adrián Gómez <agomez@orioncid.mx>
2020-03-04 23:14:52 -0600

    Commit inicial y test project

---
+++ CitasService/Citas.Test/CitaTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace Citas.Test
+{
+    public class CitaTest
+    {
+        [Fact(DisplayName = "Debería existir Cita con Datos Mínimos")]
+        public void DeberiaExistirCitaBasica()
+        {
+            // Arrange
+
+            // Act
+            var cita = new Citas();
+
+            // Assert
+            Assert.NotNull(cita);
+        }
+    }
+}
